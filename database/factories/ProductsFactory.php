cc75eac114288c5dc8e7fd444c85a54ea2c9ac14
petarkaranovic <petarkaranovic@yahoo.com>
2019-05-21 18:15:04 +0200

    Products table seeded

---
+++ database/factories/ProductsFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+$factory->define(App\Product::class, function (Faker $faker) {
+    return [
+        'name'=>$faker->company,
+        'price'=>$faker->biasedNumberBetween($min = 10, $max = 100, $function = 'sqrt'),
+        'units'=>$faker->biasedNumberBetween($min = 10, $max = 30, $function = 'sqrt'),
+        'description'=>$faker->sentence,
+        'image'=>$faker->imageUrl($width=640,$height=480),
+
+    ];
+});
