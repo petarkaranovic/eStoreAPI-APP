f3aa81144ca730e45b7bc1b826f7978482da8db7
petarkaranovic <petarkaranovic@yahoo.com>
2019-05-25 14:21:28 +0200

    single product vue created

--- resources/assets/js/views/SingleProduct.vue
+++ resources/assets/js/views/SingleProduct.vue
@@ -1,13 +1,36 @@
 <template>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <img :src='product.image' :alt="product.name">
+                <h3 class="title" v-html="product.name"></h3>
+                <p class="text-muted">{{ product.description }}</p>
+                <h4>
+                    <span class="small-text text-muted float-left"> ${{ product.price }}</span>
+                    <span class="small-text float-right">Available quantity: {{ product.units }}</span>
+                </h4>
+                <br>
+                <hr>
+                <router-link :to="{path: '/checkout?pid='+product.id}" class="col-md-4 btn btn-primary float-right">Buy now!</router-link>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            product:[]
+        }
+    },
+    beforeMount(){
+        let url=`api/products/${this.$route.params.id}`
+        axios.get(url).then(response => this.product=response.data)
+    }
 }
 </script>
-
-<style>
-
+<style scoped>
+.small-text {font-size:18px;}
+.title {font-size:36px;}
 </style>
