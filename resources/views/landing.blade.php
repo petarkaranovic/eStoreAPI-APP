0d37cd14cb048226e41a3c3c78c1aa2ee971ed97
petarkaranovic <petarkaranovic@yahoo.com>
2019-05-25 13:42:50 +0200

    crucial fix in landing blade

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -6,13 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{csrf_token()}}">
     <title>Big Store</title>
-    <link href=" {{ mix('css/app.css') }}" rel="stylesheet">
+    {{-- <link href=" {{ mix('css/app.css') }}" rel="stylesheet"> --}}
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
 </head>
 <body>
     <div id="app">
         <app></app>
     </div>
-    <script src="{{ mix('js/bootstrap.js') }}"></script>
-    <script src="{{ mix('js/app.js') }}"></script>
+    {{-- <script src="{{ mix('js/bootstrap.js') }}"></script> --}}
+    <script type="text/javascript" src="{{ asset('js/bootstrap.js') }}"></script>
+    {{-- <script src="{{ mix('js/app.js') }}"></script> --}}
+    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
