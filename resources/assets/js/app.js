db55f0a4bdaca742ce12cce7ce0475cd19ea0677
petarkaranovic <petarkaranovic@yahoo.com>
2019-05-27 16:46:12 +0200

    files alteration and fixing

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,104 +13,103 @@ import Confirmation from './views/Confirmation'
 import UserBoard from './views/UserBoard'
 import Admin from './views/Admin'
 
-const router = new VueRouter ({
-    mode:'history',
+const router = new VueRouter({
+    mode: 'history',
     routes: [
         {
-            path:'/',
-            name:'home',
+            path: '/',
+            name: 'home',
             component: Home
         },
         {
-            path:'/login',
-            name:'login',
+            path: '/login',
+            name: 'login',
             component: Login
         },
         {
-            name:'/register',
-            name:'register',
-            component:Register 
+            path: '/register',
+            name: 'register',
+            component: Register
         },
         {
-            path:'/products/:id',
-            name:'single-products',
-            component:SingleProduct 
+            path: '/products/:id',
+            name: 'single-products',
+            component: SingleProduct
         },
         {
-            path:'/confirmation',
-            name:'confirmation',
-            component:Confirmation 
+            path: '/confirmation',
+            name: 'confirmation',
+            component: Confirmation
         },
         {
-            path:'/checkout',
-            name:'checkout',
-            component:Checkout,
-            props: (route) => ({pid:route.query.pid})
+            path: '/checkout',
+            name: 'checkout',
+            component: Checkout,
+            props: (route) => ({pid : route.query.pid})
         },
         {
-            path:'/dashboard',
-            name:'userBoard',
-            component:UserBoard,
-            meta: {
+            path: '/dashboard',
+            name: 'userboard',
+            component: UserBoard,
+            meta: { 
                 requiresAuth: true,
-                is_user:true
+                is_user : true
             }
         },
         {
-            path:'/admin/:page',
-            name:'admin-pages',
-            component:Admin,
-            meta: {
-                requiresAuth : true,
+            path: '/admin/:page',
+            name: 'admin-pages',
+            component: Admin,
+            meta: { 
+                requiresAuth: true,
                 is_admin : true
             }
         },
         {
-            path:'/admin',
-            name:'admin',
-            component:Admin,
-            meta: {
-                requiresAuth : true,
-                is_admin:true 
+            path: '/admin',
+            name: 'admin',
+            component: Admin,
+            meta: { 
+                requiresAuth: true,
+                is_admin : true
             }
         },
     ],
 })
-
-router.beforeEach((to,from,next) => {
-    if (to.matched.some(record => record.meta.requiresAuth)){  
-        if(localStorage.getItem('bigStore.jwt') == null) {
-            next({
-                path:'/login',
-                params: {nextUrl: to.fullPath}
-            })
-        } else {
-            let user = JSON.parse(localStorage.getItem('bigStore.user'))
-            if(to.matched.some(record=>record.meta.is_admin)){
-                if(user.is_admin == 1 ) {
-                    next()
-                }
-                else {
-                    next({name:'userBoard'})
-                }
-            }
-            else if (to.matched.some(record => record.meta.is_user)){
-                if(user.is_admin == 0) {
-                    next()
-                }
-                else {
-                    next ({name:'admin'})
-                }
-            }
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if (localStorage.getItem('jwt') == null) {
+      next({
+        path: '/login',
+        params: { nextUrl: to.fullPath }
+      })
+    } else {
+      let user = JSON.parse(localStorage.getItem('user'))
+      if(to.matched.some(record => record.meta.is_admin)) {
+        if(user.is_admin == 1){
             next()
         }
-    } else {
-        next()
+        else{
+            next({ name: 'userboard'})
+        }
+      }
+      else if(to.matched.some(record => record.meta.is_user)) {
+        if(user.is_admin == 0){
+            next()
+        }
+        else{
+            next({ name: 'admin'})
+        }
+      }
+      next()
     }
+  } else {
+    next() 
+  }
 })
 
 const app = new Vue({
-    el :'#app',
-    components: {App},
-    router, 
+    el: '#app',
+    components: { App },
+    router
 });
